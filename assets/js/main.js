677cff7e90a9ca0fbb7d301e3a5fa8e8c54908f3
ffalt <ffalty@gmail.com>
2017-08-31 18:07:29 +0200

    smooth scrolling & avoid adding url hash

--- assets/js/main.js
+++ assets/js/main.js
@@ -278,7 +278,37 @@ function animation() {
 
 }
 
-document.addEventListener("DOMContentLoaded", function (event) {
+function smoothscoll() {
+// Add smooth scrolling on all links inside the navbar
+	$("#navbar a").on('click', function (event) {
+
+		$(this).blur();
+		// Make sure this.hash has a value before overriding default behavior
+		if (this.hash !== "") {
+
+			// Prevent default anchor click behavior
+			event.preventDefault();
+
+			// Store hash
+			var hash = this.hash;
+
+			// Using jQuery's animate() method to add smooth page scroll
+			// The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+			$('html, body').animate({
+				scrollTop: $(hash).offset().top
+			}, 800, function () {
+
+				// Add hash (#) to URL when done scrolling (default click behavior)
+				// window.location.hash = hash;
+			});
+
+		} // End if
+
+	});
+}
+
+$(document).ready(function () {
+	smoothscoll();
 	map();
 	animation();
 });
