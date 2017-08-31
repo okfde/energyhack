3a344c80c0c2c2f279d86f8408b402add20cf0d8
ffalt <ffalty@gmail.com>
2017-08-31 17:48:23 +0200

    large ascii art prepare txt->js & animation

---
+++ scripts/transform.js
@@ -0,0 +1,12 @@
+var fs = require('fs');
+
+var transform = function (nr) {
+	var lines = fs.readFileSync('ascii' + nr + '.txt').toString().split('\n');
+	return '[' + lines.map(function (line) {
+		return  JSON.stringify(line);
+	}).join(',') + ']';
+};
+
+var data = 'var asciis = [' + [transform(1), transform(2), transform(3)].join(',') + '];';
+
+fs.writeFileSync('../assets/js/ascii.js', data);
